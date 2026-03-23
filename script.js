03a2002e0ec44e723f5ea86a9ed157c53f8afe57
Nilofar2006 <nilofarjamadar10@gmail.com>
2026-03-23 21:18:22 +0530

    Add files via upload
    Delete button is added

--- script.js
+++ script.js
@@ -5,9 +5,23 @@ let list = document.getElementById("taskList");
 button.addEventListener("click", function () {
   let taskText = input.value;
 
+  if (taskText === "") {
+    return;
+  }
+
   let li = document.createElement("li");
 
-  li.innerText = taskText;
+  let deleteBtn = document.createElement("button");
+
+  deleteBtn.innerText = "Delete";
+
+  li.innerText = taskText + "";
+
+  li.appendChild(deleteBtn);
+
+  deleteBtn.addEventListener("click", function () {
+    li.remove();
+  });
 
   list.appendChild(li);
 
